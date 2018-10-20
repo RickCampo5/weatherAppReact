67c316755d2eece0e8dc69be37a0c95da38ca025
Ricardo Campos <ricamposm@gmail.com>
2018-10-20 11:36:50 -0500

    Funacional

--- src/components/ForecastExtended.js
+++ src/components/ForecastExtended.js
@@ -1,36 +1,60 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import transformForecast from '../services/transformForecast'
 import ForecastItem from './ForecastItem'
-
-const days = [
-  'Lunes',
-  'Martes',
-  'Miercoles',
-  'Jueves',
-  'Viernes',
-  'Sabado',
-  'Domingo'
-]
-
-const data = {
-  temperature: 10,
-  humidity: 10,
-  weatherState: 'normal',
-  wind: "normal"
-}
+import { url_base_forecast, api_key } from '../constants/api_url'
 
 class ForecastExtended extends Component {
 
-  renderForecastItemsDays(){
-    return days.map( day => <ForecastItem weekDay={day} hour={10} data={data}></ForecastItem>) 
+  constructor(){
+    super();
+    this.state = { forecastData: null }
+  }
+
+  componentDidMount(){
+    this.updateCity(this.props.city);
+  }
+
+  componentWillReceiveProps(nextProps){
+    if(nextProps !== this.props.city){
+      this.setState({ forecastData: null });
+      this.updateCity(nextProps.city);
+    }
+  }
+
+  updateCity = city => {
+    const url_forecast  = `${url_base_forecast}?q=${city}&appid=${api_key}&units=metric`;
+
+    fetch(url_forecast)
+    .then(data =>
+      (data.json())
+    )
+    .then(weather_data =>{
+      console.log(weather_data)
+      const forecastData = transformForecast(weather_data)
+      console.log(forecastData)
+      this.setState({ forecastData })
+    })
+  }
+
+  renderForecastItemsDays(forecastData){
+    return forecastData.map( forecast => 
+      <ForecastItem key={`${forecast.weekDay}${forecast.hour}`} weekDay={forecast.weekDay} hour={forecast.hour} data={forecast.data}></ForecastItem>) 
+  }
+
+  renderProgress(){
+    return <h3>"Cargando Pronóstico Extendido..."</h3>;
   }
 
   render(){
     const { city } = this.props;
+    const { forecastData } = this.state;
     return (
       <div>
         <h2 className="forecastTitle">Pronostio Extendido para {city}</h2>
-        {this.renderForecastItemsDays()}
+        {forecastData ? 
+        this.renderForecastItemsDays(forecastData) : 
+        this.renderProgress()}
       </div>
     )
   }
--- src/constants/api_url.js
+++ src/constants/api_url.js
@@ -1,2 +1,3 @@
 export const api_key = "783c31469606395782e2db540febeef7";
 export const url_base_weather = "http://api.openweathermap.org/data/2.5/weather";
+export const url_base_forecast = "http://api.openweathermap.org/data/2.5/forecast"
